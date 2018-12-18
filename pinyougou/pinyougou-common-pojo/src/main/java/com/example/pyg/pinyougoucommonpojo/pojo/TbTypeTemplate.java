db146fccb21e0e44614605d2579ca3d733cf66e1
zhangshanbaoKAKE <13663565840@163.com>
2018-12-18 14:11:19 +0800

    类型模板：用于关联品牌和规格   提交

---
+++ pinyougou/pinyougou-common-pojo/src/main/java/com/example/pyg/pinyougoucommonpojo/pojo/TbTypeTemplate.java
@@ -0,0 +1,83 @@
+package cn.pojo;
+
+public class TbTypeTemplate {
+    private Long id;
+
+    private String name;
+
+    private String specIds;
+
+    private String brandIds;
+
+    private String customAttributeItems;
+
+    private String ex1;
+
+    private String ex2;
+
+    private String ex3;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSpecIds() {
+        return specIds;
+    }
+
+    public void setSpecIds(String specIds) {
+        this.specIds = specIds == null ? null : specIds.trim();
+    }
+
+    public String getBrandIds() {
+        return brandIds;
+    }
+
+    public void setBrandIds(String brandIds) {
+        this.brandIds = brandIds == null ? null : brandIds.trim();
+    }
+
+    public String getCustomAttributeItems() {
+        return customAttributeItems;
+    }
+
+    public void setCustomAttributeItems(String customAttributeItems) {
+        this.customAttributeItems = customAttributeItems == null ? null : customAttributeItems.trim();
+    }
+
+    public String getEx1() {
+        return ex1;
+    }
+
+    public void setEx1(String ex1) {
+        this.ex1 = ex1 == null ? null : ex1.trim();
+    }
+
+    public String getEx2() {
+        return ex2;
+    }
+
+    public void setEx2(String ex2) {
+        this.ex2 = ex2 == null ? null : ex2.trim();
+    }
+
+    public String getEx3() {
+        return ex3;
+    }
+
+    public void setEx3(String ex3) {
+        this.ex3 = ex3 == null ? null : ex3.trim();
+    }
+}
